9e54a04a25f5f4e9536d6681a69033ab0536a0dd
Dirk van der Walt <dirkvnderwalt@gmail.com>
2023-05-01 18:21:58 +0000

    More work on Firewall Profiles

--- cake4/rd_cake/src/Controller/Component/MdFirewallComponent.php
+++ cake4/rd_cake/src/Controller/Component/MdFirewallComponent.php
@@ -10,6 +10,7 @@ class MdFirewallComponent extends Component {
 	protected $Hardware = 'tplink'; //Some default value
 	protected $ApId     = '';
 	protected $RadioSettings = [];
+	protected $Sets 	= [];
 
 	public function initialize(array $config):void{
         $this->Nodes 		= TableRegistry::get('Nodes');
@@ -17,11 +18,20 @@ class MdFirewallComponent extends Component {
         $this->Meshes		= TableRegistry::get('Meshes');
         $this->ApProfiles	= TableRegistry::get('ApProfiles');
         $this->Hardwares	= TableRegistry::get('Hardwares');
+        $this->FirewallProfiles	= TableRegistry::get('FirewallProfiles');
+        
+        $this->Sets[-1] = [
+        	'name' 		=> 'md_pvt_addr',
+        	'elements' 	=> '10.0.0.0/8,172.16.0.0/12,192.168.0.0/16',
+        	'comment'	=> 'Private IP Addresses'      
+        ];       
     }
     
     public function JsonForMac($mac){
     
     	$firewall_list = [];
+    	$firewall_list['entries'] = [];
+    	$firewall_list['sets']  = []; 
     	
     	$e_ap = $this->{'Aps'}->find()->where(['Aps.mac' => $mac])->contain(['ApProfiles.ApProfileExits'])->first(); //First try for AP
     	if($e_ap){
@@ -39,32 +49,94 @@ class MdFirewallComponent extends Component {
 					]
 				]
 			]);
-           	$ap_profile = $query->where(['ApProfiles.id' => $ap_profile_id,'Aps.mac' =>$mac])->first();
-           	
-           	$firewall_list = $this->_apMdFw($ap_profile);
+           	$ap_profile = $query->where(['ApProfiles.id' => $ap_profile_id,'Aps.mac' =>$mac])->first();      	
            	$this->_setWiFiSettings();
-    	
+           	//Determine the radio count and configure accordingly
+		    $radios = 0;
+			$q_e = $this->{'Hardwares'}->find()
+				->where(['Hardwares.fw_id' => $this->Hardware,'Hardwares.for_ap' => true])
+				->first();
+			if($q_e){
+			    $radios = $q_e->radio_count;
+			}       	           	       	
+           	$temp_list = $this->_apMdFw($ap_profile,$radios);
+           	foreach($temp_list as $fw){
+           		$fw['rules'] = $this->_fwRulesFor($fw);
+           		array_push($firewall_list['entries'],$fw);       		       	
+           	} 
+           	$firewall_list['sets']  = $this->Sets;          	  	
     	
     	}else{
     	
     		$e_node 	= $this->Nodes->find()->where(['Nodes.mac' => $mac])->contain(['Meshes'])->first(); //If not try for Mesh Nodes
-    		print_r($e_node);
-    	   	
-    	
+    	   	    	
     	}
     	   	
     	return $firewall_list;
     }
     
     
-    private function _apMdFw($ap_profile){
+    private function _fwRulesFor($fw_entry){
+      	
+  		$rules = [];
+    	$fw_entry_id = $fw_entry['firewall_profile_id'];
+    	$fw_profile = $this->{'FirewallProfiles'}->find()
+    		->contain(['FirewallProfileEntries'=>['FirewallProfileEntryFirewallApps'=> 'FirewallApps']])
+    		->where(['FirewallProfiles.id' => $fw_entry_id])
+    		->first();		
+    		
+    	if($fw_profile){   
+    		foreach($fw_profile->firewall_profile_entries as $rule){		
+    			unset($rule->created);
+    			unset($rule->modified);
+    			
+    			if($rule->category == 'app'){
+    				$rule->apps = [];
+    				foreach($rule->firewall_profile_entry_firewall_apps as $fw_app){
+    					$app_name = $fw_app->firewall_app->name;
+    					
+    					if(!isset($this->Sets[$fw_app->firewall_app->id])){
+    						unset($fw_app->firewall_app->created);
+    						unset($fw_app->firewall_app->modified); 
+    						unset($fw_app->firewall_app->fa_code);  
+    						unset($fw_app->firewall_app->cloud_id);					
+    						$this->Sets[$fw_app->firewall_app->id] = $fw_app->firewall_app;	   					
+    					}
+    					
+    					array_push($rule->apps,$app_name); 				
+    				}
+    			}
+    			
+    			unset($rule->firewall_profile_entry_firewall_apps);
+    			
+    			array_push($rules,$rule) ;  		
+    		}	  			
+    	}
+    	return $rules;      
+    }
     
-    	$network 	= [];
-    	$br_int 	= $this->_ap_wan_for($this->Hardware);
-    	$wan_if 	= $br_int;
+    
+    private function _apMdFw($ap_profile,$radio_count){
+    
+    	$tmp_network = [];
+    	$network 	 = [];
+    	$fw_flag	 = false;
     	
-        $entry_point_data = [];  
-        $start_number = 0;
+    	//--Check if there are any active firewalls / if none return --
+    	foreach($ap_profile->ap_profile->ap_profile_exits as $e_exit){
+    		if($e_exit->apply_firewall_profile){
+    			$fw_flag = true;
+    			break;
+    		}
+    	}
+    	
+    	if(!$fw_flag){
+    		return [];
+    	}
+    		
+        $entry_point_data 	= [];  
+        $start_number 		= 0;
+        $interface_data		= [];
                
         foreach($ap_profile->ap_profile->ap_profile_exits as $ap_profile_e){
             $has_entries_attached   = false;
@@ -97,7 +169,7 @@ class MdFirewallComponent extends Component {
                     }
                 }           
             }
-            
+
             if($has_entries_attached == true){
             	if($ap_profile_e->type !== 'bridge'){          
             		$ap_profile_e->network = $if_name;             		
@@ -105,12 +177,71 @@ class MdFirewallComponent extends Component {
             	}else{
             		$ap_profile_e->network = 'lan';
             	}
-            	array_push($network,$ap_profile_e);           
+            	
+            	$interface_data[$ap_profile_e->network] = []; //Create an empty entry (which will be polulated later)
+            	if($eth_one_bridge){
+            		$interface_data[$ap_profile_e->network] = $this->_lan_for($this->Hardware);          		
+            	}
+            	
+            	if($ap_profile_e->apply_firewall_profile){
+            		$fw_entry = [
+            			'type'					=> $ap_profile_e->type,
+            			'firewall_profile_id'	=> $ap_profile_e->firewall_profile_id,
+            			'network'				=> $ap_profile_e->network,
+            			//'interfaces'			=> []     //There's no interfaces right now we add it only later   		
+            		];
+            		array_push($tmp_network,$fw_entry);
+            	}            	           
             }          
       	}
-      	
-      	print_r($entry_point_data);
-    
+      	  	
+      	$start_number = 0;     	
+      	foreach($ap_profile->ap_profile->ap_profile_entries as $ap_profile_e){
+            $entry_id   = $ap_profile_e->id;
+            foreach($entry_point_data as $epd){
+                if($epd['entry_id'] == $entry_id){ //We found our man :-) This means the Entry has been 'connected' to an exit point
+                
+                	$network_name = $epd['network'];
+
+                	//Now we need to determine the WiFi Interface names
+                	for ($y = 0; $y < $radio_count; $y++){
+                	
+                		if(
+                			($ap_profile_e->frequency_band == 'five_upper')||
+                			($ap_profile_e->frequency_band == 'five_lower')||
+                			($ap_profile_e->frequency_band == 'five')||
+                			($ap_profile_e->frequency_band == 'both')
+                		){                 		
+                			if((!$this->RadioSettings[$y]['disabled'])&&($this->RadioSettings[$y]['band'] == '5g' )){
+                				$if_name    = $this->_number_to_word($start_number);
+				        		$if_name    = "$if_name"."$y";
+				        		array_push($interface_data[$network_name],$if_name);
+                				$start_number++;
+                			}                			              		
+                		}
+                		
+                		if(
+                			($ap_profile_e->frequency_band == 'two')||
+                			($ap_profile_e->frequency_band == 'both')
+                		){ 
+                		
+                			if((!$this->RadioSettings[$y]['disabled'])&&($this->RadioSettings[$y]['band'] == '2g' )){
+                				$if_name    = $this->_number_to_word($start_number);
+				        		$if_name    = "$if_name"."$y";
+				        		array_push($interface_data[$network_name],$if_name);
+                				$start_number++; 
+                			}                 			             		
+                		}                	                		
+                	}               	                	
+                }
+            }           
+     	}
+     	
+     	foreach($tmp_network as $n){     	
+     		$n['interfaces'] = $interface_data[$n['network']]; 	
+     		array_push($network,$n);	
+     	}
+     	    
     	return $network;
     }
     
@@ -161,9 +292,8 @@ class MdFirewallComponent extends Component {
         if($q_e){
             foreach($q_e->hardware_radios as $hr){    
                 $radio_number   = $hr->radio_number;
-                $prefix = 'radio'.$radio_number.'_';
                 foreach($radio_fields as $fr){                    
-                 	$this->RadioSettings[$radio_number]["$prefix$fr"] = $hr->{"$fr"};
+                 	$this->RadioSettings[$radio_number]["$fr"] = $hr->{"$fr"};
                 }
             }
         }
@@ -189,6 +319,8 @@ class MdFirewallComponent extends Component {
                         $radio_number = 2;
                     }
                     
+                    $name = str_replace('radio'.$radio_number.'_','',$name);
+                    
                     //Small fix to make boolean from string....
                     if($value == 'false'){
                         $value = false;
@@ -204,5 +336,18 @@ class MdFirewallComponent extends Component {
             }
         }
     }
+    
+   	private function _lan_for($hw){
+	    $return_val = ['eth1']; //some default	
+		$q_e = $this->{'Hardwares'}->find()->where(['Hardwares.fw_id' => $hw, 'Hardwares.for_ap' => true])->first();
+		if($q_e){
+		    $return_val = [$q_e->lan]; 
+		    $ports = preg_split('/\s+/', $q_e->lan);//New format if there are multiple items
+		    if($ports){
+		    	$return_val = $ports; 
+		    } 
+		}
+		return $return_val;
+	}
 
 }
