492e05ac90689ef491ef6b00cb5236e53d64482f
Dirk van der Walt <dirkvnderwalt@gmail.com>
2023-05-09 12:58:31 +0000

    Fix broken Git Step2

---
+++ rd/classic/src/view/bans/vcBans.js
@@ -0,0 +1,221 @@
+Ext.define('Rd.view.bans.vcBans', {
+    extend  : 'Ext.app.ViewController',
+    alias   : 'controller.vcBans',
+        init    : function() {
+    
+    },
+    config: {
+    	UrlDelete	: '/cake4/rd_cake/bans/delete.json',
+        UrlAdd		: '/cake4/rd_cake/bans/add.json',
+        UrlEdit		: '/cake4/rd_cake/bans/edit.json'
+    },
+    control: {
+        'gridBans #reload': {
+            click   : 'reload'
+        },
+        'gridBans #add': {
+            click   : 'add'
+        },
+        'gridBans #edit': {
+            click   : 'edit'
+        },      
+        'gridBans #delete': {
+            click   : 'del'
+        },
+        'winAddBan #save': {
+            click: 'addSave'
+        },
+        'winEditBan #save': {
+            click: 'editSave'
+        }
+    },
+    reload: function(){
+        var me      = this;
+        me.getView().getSelectionModel().deselectAll(true);
+        me.getView().getStore().load();
+    },
+    del:   function(){
+        var me      = this;     
+        //Find out if there was something selected
+        if(me.getView().getSelectionModel().getCount() == 0){
+             Ext.ux.Toaster.msg(
+                        i18n('sSelect_an_item'),
+                        i18n('sFirst_select_an_item_to_delete'),
+                        Ext.ux.Constants.clsWarn,
+                        Ext.ux.Constants.msgWarn
+            );
+        }else{
+            Ext.MessageBox.confirm(i18n('sConfirm'), i18n('sAre_you_sure_you_want_to_do_that_qm'), function(val){
+                if(val== 'yes'){
+                    var selected    = me.getView().getSelectionModel().getSelection();
+                    var list        = [];
+                    Ext.Array.forEach(selected,function(item){
+                        var id = item.get('id');
+                        Ext.Array.push(list,{'id' : id});
+                    });
+                    Ext.Ajax.request({
+                        url: me.getUrlDelete(),
+                        method: 'POST',          
+                        jsonData: list,
+                        success: function(batch,options){console.log('success');
+                            Ext.ux.Toaster.msg(
+                                i18n('sItem_deleted'),
+                                i18n('sItem_deleted_fine'),
+                                Ext.ux.Constants.clsInfo,
+                                Ext.ux.Constants.msgInfo
+                            );
+                            me.reload(); //Reload from server
+                        },                                    
+                        failure: function (response, options) {
+                            var jsonData = Ext.JSON.decode(response.responseText);
+                            Ext.Msg.show({
+                                title       : "Error",
+                                msg         : response.request.url + '<br>' + response.status + ' ' + response.statusText+"<br>"+jsonData.message,
+                                modal       : true,
+                                buttons     : Ext.Msg.OK,
+                                icon        : Ext.Msg.ERROR,
+                                closeAction : 'destroy'
+                            });
+                            me.reload(); //Reload from server
+                        }
+                    });
+                }
+            });
+        }
+    },
+    onViewActivate: function(pnl){
+        var me = this;
+        me.reload();   
+    },
+    add: function(btn){
+    	var me = this;
+    	if(!Ext.WindowManager.get('winAddBanId')){
+            var w = Ext.widget('winAddBan',{id:'winAddBanId'});
+            me.getView().add(w); 
+            let appBody = Ext.getBody();
+            w.showBy(appBody);           
+        }  
+    },
+    edit: function(btn){
+    	var me = this;
+        if(me.getView().getSelectionModel().getCount() == 0){
+            Ext.ux.Toaster.msg(
+                        i18n('sSelect_an_item'),
+                        i18n('sFirst_select_an_item'),
+                        Ext.ux.Constants.clsWarn,
+                        Ext.ux.Constants.msgWarn
+            );          
+        }else{
+            var sr      = me.getView().getSelectionModel().getLastSelected();  			
+			if(!Ext.WindowManager.get('winEditBanId')){
+                var w = Ext.widget('winEditBan',{id:'winEditBanId',record: sr});
+                me.getView().add(w); 
+                let appBody = Ext.getBody();
+                w.showBy(appBody);      
+            }
+        }
+    },
+    rgrpScopeChange : function( rgrp,newValue, oldValue, eOpts){
+
+		var form = rgrp.up('form');
+		var cmbMesh = form.down('#mesh_id');
+		var cmbApProfile = form.down('#ap_profile_id');
+		
+		if(newValue.scope == 'cloud_wide'){
+			cmbMesh.setHidden(true);
+			cmbMesh.setDisabled(true);
+			cmbApProfile.setHidden(true);
+			cmbApProfile.setDisabled(true);			
+		}
+		
+		if(newValue.scope == 'ap_profile_only'){
+			cmbMesh.setHidden(true);
+			cmbMesh.setDisabled(true);
+			cmbApProfile.setHidden(false);
+			cmbApProfile.setDisabled(false);				
+		}
+		
+		if(newValue.scope == 'mesh_only'){
+			cmbMesh.setHidden(false);
+			cmbMesh.setDisabled(false);
+			cmbApProfile.setHidden(true);
+			cmbApProfile.setDisabled(true);				
+		}   
+    },
+    rgrpActionChange : function( rgrp,newValue, oldValue, eOpts){
+    	var form 	= rgrp.up('form');
+    	var bw_up 	= form.down('#bw_up');
+    	var bw_down = form.down('#bw_down');
+    	var cmb_fw  = form.down('cmbFirewallProfile');
+    	if(newValue.action == 'block'){
+    		bw_up.setHidden(true);
+			bw_up.setDisabled(true);
+			bw_down.setHidden(true);
+			bw_down.setDisabled(true);
+			cmb_fw.setHidden(true);
+			cmb_fw.setDisabled(true);			 	
+    	}
+    	
+    	if(newValue.action == 'limit'){
+    		bw_up.setHidden(false);
+			bw_up.setDisabled(false);
+			bw_down.setHidden(false);
+			bw_down.setDisabled(false);
+			cmb_fw.setHidden(true);
+			cmb_fw.setDisabled(true);	
+    	}
+    	
+    	if(newValue.action == 'firewall'){
+    		bw_up.setHidden(true);
+			bw_up.setDisabled(true);
+			bw_down.setHidden(true);
+			bw_down.setDisabled(true);
+			cmb_fw.setHidden(false);
+			cmb_fw.setDisabled(false);
+    	}     	    
+    },
+    addSave: function(btn){
+        var me 		= this;
+        var form    = btn.up('form');
+        var window  = form.up('window'); 
+        form.submit({
+            clientValidation: true,
+            url             : me.getUrlAdd(),
+            success         : function(form, action) {              
+                //FIXME reload store....
+                Ext.ux.Toaster.msg(
+                    'Item Added',
+                    'Item Added Fine',
+                    Ext.ux.Constants.clsInfo,
+                    Ext.ux.Constants.msgInfo
+                );               
+                me.reload();
+                window.close();
+            },
+            failure: Ext.ux.formFail,
+            scope: me
+        });
+    },
+    editSave: function(btn){
+        var me 		= this;
+        var form    = btn.up('form');
+        var window  = form.up('window'); 
+        form.submit({
+            clientValidation: true,
+            url             : me.getUrlEdit(),
+            success         : function(form, action) {              
+                //FIXME reload store....
+                Ext.ux.Toaster.msg(
+                    'Item Updated',
+                    'Item Updated Fine',
+                    Ext.ux.Constants.clsInfo,
+                    Ext.ux.Constants.msgInfo
+                );               
+                me.reload();
+                window.close();
+            },
+            failure: Ext.ux.formFail,
+            scope: me
+        });
+    }
+});
---
+++ rd/classic/src/view/bans/winEditBan.js
@@ -0,0 +1,208 @@
+Ext.define('Rd.view.bans.winEditBan', {
+    extend      : 'Ext.window.Window',
+    alias       : 'widget.winEditBan',
+    closable    : true,
+    draggable   : true,
+    resizable   : true,
+    title       : 'Edit Block Or Speed Limit',
+    width       : 550,
+    height      : 500,
+    plain       : true,
+    border      : false,
+    layout      : 'fit',
+    glyph       : Rd.config.icnEdit,
+    autoShow    :   false,
+    defaults: {
+            border: false
+    },
+    requires: [
+    ],
+    initComponent: function() {
+        var me      = this;
+        
+        var cmbApProfile = Ext.create('Rd.view.components.cmbApProfile',{
+		    itemId      : 'ap_profile_id',
+		    hidden		: true,
+		    disabled	: true
+	    });
+	    
+	    var cmbMesh = Ext.create('Rd.view.components.cmbMesh',{
+		    itemId      : 'mesh_id',
+		    hidden		: true,
+		    disabled	: true
+	    });
+	    
+	    var sldrUpload = Ext.create('Rd.view.components.rdSliderSpeed',{
+            xtype       : 'rdSliderSpeed',
+            sliderName  : 'limit_upload',
+            itemId		: 'bw_up',
+            fieldLabel  : "<i class='fa fa-arrow-up'></i> Up",
+            hidden		: true,
+            disabled	: true
+        });
+        
+        var sldrDownload = Ext.create('Rd.view.components.rdSliderSpeed',{
+            xtype       : 'rdSliderSpeed',
+            sliderName  : 'limit_download',
+            itemId		: 'bw_down',
+            fieldLabel  : "<i class='fa fa-arrow-up'></i> Up",
+            hidden		: true,
+            disabled	: true
+        });
+        
+        var cmbFirewall = Ext.create('Rd.view.components.cmbFirewallProfile',{
+        	fieldLabel	: 'Firewall Profile',
+        	include_all_option : false,
+        	disabled	: true,
+        	hidden		: true,
+        	labelClsExtra: 'lblRdReq'       
+        });
+	    
+	    var cloud_id	= me.record.get('cloud_id');
+	    var mesh_id		= me.record.get('mesh_id');
+	    var ap_profile_id = me.record.get('ap_profile_id');
+	    var	rb_cloud	= true;
+	    var rb_mesh		= false;
+	    var rb_ap_p		= false;
+	    
+	    var action		= me.record.get('action');
+	    var rb_block	= true;
+	    var rb_limit	= false;
+	    var rb_firewall = false;
+	    
+	    if(action == 'limit'){
+	    	rb_block	= false;
+	        rb_limit	= true;
+	        sldrUpload.setHidden(false);
+	    	sldrUpload.setDisabled(false);
+	        sldrDownload.setHidden(false);
+	    	sldrDownload.setDisabled(false);
+	    	
+	    	sldrUpload.down('numberfield').setValue(me.record.get('bw_up'));
+	    	sldrDownload.down('numberfield').setValue(me.record.get('bw_down'));
+	    	sldrUpload.down('combobox').setValue(me.record.get('bw_up_suffix'));
+	    	sldrDownload.down('combobox').setValue(me.record.get('bw_down_suffix'));
+	    	    	
+	    }  
+	    
+	    if(action == 'firewall'){
+	    	rb_block	= false;
+	        rb_firewall = true;
+	        cmbFirewall.setHidden(false);
+	    	cmbFirewall.setDisabled(false);	
+	    	cmbFirewall.setValue(me.record.get('firewall_profile_id'));	    	    	
+	    }   
+	     	    
+	    if(mesh_id > 0){
+	    	rb_cloud	= false;
+	    	rb_mesh		= true;
+	    	rb_ap_p		= false;
+	    	cmbMesh.setHidden(false);
+	    	cmbMesh.setDisabled(false);
+	    	cmbMesh.getStore().load();
+			cmbMesh.setValue(mesh_id);//Show it	 	
+	    }
+	    
+	    if(ap_profile_id > 0){
+	    	rb_cloud	= false;
+	    	rb_mesh		= false;
+	    	rb_ap_p		= true;
+	    	cmbApProfile.setHidden(false);
+	    	cmbApProfile.setDisabled(false);
+	    	cmbApProfile.getStore().load();
+			cmbApProfile.setValue(ap_profile_id);//Show it	    	
+	    }
+               
+        var frmData = Ext.create('Ext.form.Panel',{
+            border:     false,
+            layout:     'anchor',
+            autoScroll: true,
+            defaults: {
+                anchor: '100%'
+            },
+            fieldDefaults: {
+                msgTarget       : 'under',
+                labelClsExtra   : 'lblRd',
+                labelAlign      : 'left',
+                labelSeparator  : '',
+                labelClsExtra   : 'lblRd',
+                margin          : Rd.config.fieldMargin
+            },
+            defaultType: 'textfield',
+            buttons: [
+                {
+                    itemId      : 'save',
+                    formBind    : true,
+                    text        : 'SAVE',
+                    scale       : 'large',
+                    glyph       : Rd.config.icnYes,
+                    margin      : Rd.config.buttonMargin,
+                    ui          : 'button-teal'
+                }
+            ],
+            items: [
+            
+            	 {
+                    xtype       :  'hiddenfield',
+                    name        :   'id',
+                    hidden      : true,
+                    value		: me.record.get('id')
+                },            	
+				{
+                    name        : 'mac',
+                    fieldLabel  : 'MAC',
+                    allowBlank  : false,
+                    blankText   : 'Specify A MAC Address',
+					vtype       : 'MacColon',
+					fieldStyle  : 'text-transform:uppercase',
+                    itemId      : 'txtMac',
+                    margin      : Rd.config.fieldMargin +5,
+                    labelClsExtra   : 'lblRdReq',
+                    value		: me.record.get('mac')
+                },
+				{
+                    name        : 'alias',
+                    fieldLabel  : 'Alias',
+                    allowBlank  : true,
+                    blankText   : 'Specify An Unique Alias',
+                    itemId      : 'txtAlias',
+                    margin      : Rd.config.fieldMargin +5,
+                    value		: me.record.get('alias')
+                },
+                {
+					xtype		: 'radiogroup',
+					columns		: 3,
+					vertical	: false,
+					items		: [
+						{ boxLabel: 'Cloud Wide', 	name: 'scope', inputValue: 'cloud_wide', checked: rb_cloud,	margin: 2 },
+						{ boxLabel: 'Mesk Network', name: 'scope', inputValue: 'mesh_only',checked: rb_mesh, 	margin: 2},
+						{ boxLabel: 'AP Profile',  	name: 'scope', inputValue: 'ap_profile_only', checked: rb_ap_p, margin	: 2}
+					],
+					listeners   : {
+				        change  : 'rgrpScopeChange'
+			        }
+				},
+				cmbApProfile,
+				cmbMesh,
+                {
+					xtype		: 'radiogroup',
+					columns		: 3,
+					vertical	: false,
+					items		: [
+						{ boxLabel: 'Block', 	   name: 'action', inputValue: 'block', checked: rb_block ,margin: 2 },
+						{ boxLabel: 'Speed Limit', name: 'action', inputValue: 'limit', checked: rb_limit ,margin: 2 },
+						{ boxLabel: 'Firewall Profile', name: 'action', inputValue: 'firewall', checked: rb_firewall ,margin: 2 }
+					],
+					listeners   : {
+				        change  : 'rgrpActionChange'
+			        }
+				},
+				sldrUpload,
+                sldrDownload,
+                cmbFirewall                
+            ]
+        });
+        me.items = frmData; 
+        me.callParent(arguments);
+    }
+});
