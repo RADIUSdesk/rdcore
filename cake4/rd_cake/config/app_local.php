fe4877d278336ea7e9590cf760051cb5ff7e4227
Dirk van der Walt <dirkvanderwalt@gmail.com>
2026-06-09 18:34:00 +0000

    Revert quoteIdentifiers to fix to_date error on MariaDB on some deployments it breaks other views

--- cake4/rd_cake/config/app_local.php
+++ cake4/rd_cake/config/app_local.php
@@ -54,7 +54,7 @@ return [
             
              // JUN 2026 Enable identifier quoting here - some installs broke because of the 'to_date' field interpreted as a function
              // This setting **might** have a negative impact on performance 
-            'quoteIdentifiers' => true, 
+            //'quoteIdentifiers' => true, 
             
             'cacheMetadata' => true,
             'log'	        => false,
